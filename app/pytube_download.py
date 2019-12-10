0ec1467d105ff81288c77fd6ef1eeaa724cd0b5d
Fishman Boaz <manicqin@gmail.com>
2019-12-10 16:19:44 +0200

    Update pytube_download.py

--- app/pytube_download.py
+++ app/pytube_download.py
@@ -3,7 +3,7 @@ import pytube
 def main():
     url = "https://www.youtube.com/watch?v=s6Zf76tN48A"
     yt = pytube.YouTube(url)
-    print("Going to downloaf {}".format(url))
+    print("Going to download {}".format(url))
     fname = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
     print("Downloaded video to {}".format(fname))
 
